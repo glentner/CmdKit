622276eec8a4312e74f90267c184b83bda97eb4f
Geoffrey Lentner <glentner@purdue.edu>
2019-07-02 10:24:42 -0400

    bump version to 0.0.2

--- cmdkit/__meta__.py
+++ cmdkit/__meta__.py
@@ -12,7 +12,7 @@
 
 
 __appname__   = 'cmdkit'
-__version__   = '0.0.1'
+__version__   = '0.0.2'
 __authors__   = 'Geoffrey Lentner'
 __contact__   = '<glentner@gmail.com>'
 __license__   = 'Apache License'
