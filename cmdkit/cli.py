52856187e1a1af312dd5059fb6dbc45239b4cf86
Geoffrey Lentner <glentner@purdue.edu>
2020-03-23 13:40:28 -0400

    allow parameter forwarding to ArgumentParser
    Allow for arbitrary keyword argument forwarding to the ArgumentParser.
    Lets the developer pick behavior, e.g., `allow_abbrev=False`.

--- cmdkit/cli.py
+++ cmdkit/cli.py
@@ -56,24 +56,29 @@ class Interface(_argparse.ArgumentParser):
         >>> interface.add_argument('--verbose', action='store_true')
     """
 
-    def __init__(self, program: str, usage_text: str, help_text: str) -> None:
+    def __init__(self, program: str, usage_text: str, help_text: str, **kwargs) -> None:
         """
         Explicitly provide `usage_text` and `help_text`.
 
+        Arguments
+        ---------
         program: str
             Name of program (e.g., `os.path.basename(sys.argv[0])`).
+
         usage_text: str
             Full text of program "usage" statement.
+
         help_text: str
             Full text of program "help" statement.
 
-        See Also:
+        See Also
+        --------
         `argparse.ArgumentParser`
         """
         self.program = program
         self.usage_text = usage_text
         self.help_text = help_text
-        super().__init__(prog=program, usage=usage_text, allow_abbrev=False)
+        super().__init__(prog=program, usage=usage_text, **kwargs)
 
     # prevents base class from trying to build up usage text
     def format_help(self) -> str:
