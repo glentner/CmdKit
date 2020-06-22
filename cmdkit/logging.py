5b3acce4b108ce3e7aefd5285a76eb718f650c78
Geoffrey Lentner <glentner@purdue.edu>
2020-06-22 10:00:26 -0400

    fix logging handler

--- cmdkit/logging.py
+++ cmdkit/logging.py
@@ -9,48 +9,47 @@
 # If not, see <https://www.apache.org/licenses/LICENSE-2.0>.
 
 """
-Internal logging.
+Logging for cmdkit.
 
-Add `logalpha.handlers.Handler` instances to the `cmdkit.logging.log`
-instance to pick up messages from `cmdkit` events.
+Add :class:`~logalpha.handlers.Handler` instances to the `cmdkit.logging.log`
+instance to pick up messages from :mod:`cmdkit` events (e.g., argument errors).
 
 Or, inject a custom logger by assigning it back to the module.
 Be sure to include the logger used in the `Application` class.
 
-Example
--------
->>> from cmdkit import logging as _cmdkit_logging
->>> from cmdkit.app import Application
->>> _cmdkit_logging.log = log
->>> Application.log_error = log.critical
+Example:
+    >>> from cmdkit import logging as _cmdkit_logging
+    >>> from cmdkit.app import Application
+    >>> _cmdkit_logging.log = log
+    >>> Application.log_error = log.critical
 """
-
 
 # standard libs
 import io
 import sys
 from dataclasses import dataclass
-from datetime import datetime
 
 # external libs
 from logalpha.colors import Color
-from logalpha.levels import LEVELS
+from logalpha.levels import Level, LEVELS
 from logalpha.loggers import Logger
 from logalpha.handlers import Handler
 from logalpha.messages import Message
 
 
-# add handlers to pick up messages
-log = Logger()
-
-
 @dataclass
 class ConsoleHandler(Handler):
-    """Colorized handler to <stderr>."""
+    """Colorized handler writes to <stderr>."""
 
+    level: Level = LEVELS[2]  # Level(value=2, name='WARNING')
     resource: io.TextIOWrapper = sys.stderr
 
     def format(self, msg: Message) -> str:
         return (f'{Logger.colors[msg.level.value].foreground}'
                 f'{msg.level.name}{Color.reset} '
                 f'{msg.content}')
+
+
+# add handlers to pick up messages
+log = Logger()
+log.handlers.append(ConsoleHandler())
