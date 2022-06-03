e7d6006cedf01cec35398f7b751b21a4ef7ad480
Geoffrey Lentner <glentner@purdue.edu>
2022-06-03 15:15:01 -0400

    Add Python 3.10 and 3.11 to testing matrix

--- setup.py
+++ setup.py
@@ -53,6 +53,7 @@ setup(
                         'Programming Language :: Python :: 3.8',
                         'Programming Language :: Python :: 3.9',
                         'Programming Language :: Python :: 3.10',
+                        'Programming Language :: Python :: 3.11',
                         'License :: OSI Approved :: Apache Software License', ],
     entry_points     = {'console_scripts': []},
     install_requires = DEPENDENCIES,
