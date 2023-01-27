b521182ed1453afc1597e869ce4bfdbf51137770
Geoffrey Lentner <glentner@purdue.edu>
2023-01-26 21:53:26 -0500

    Migrate to pyproject.toml and poetry

--- src/cmdkit/__init__.py
+++ src/cmdkit/__init__.py
@@ -11,10 +11,4 @@ import logging
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
 # package metadata
-__pkgname__   = 'cmdkit'
 __version__   = '2.6.1'
-__authors__   = 'Geoffrey Lentner'
-__contact__   = 'glentner@purdue.edu'
-__license__   = 'Apache License'
-__copyright__ = 'Geoffrey Lentner 2019-2022'
-__description__ = 'A command-line utility toolkit for Python.'
