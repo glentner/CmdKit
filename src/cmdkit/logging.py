ee97c146ccb735308438012b468fb46837ae1e12
Geoffrey Lentner <glentner@purdue.edu>
2023-09-27 13:20:22 -0400

    Add logging features
    - Re-export logging levels
    - Add styles mapping
    - Add new attributes (relative time, shortened names)

--- src/cmdkit/logging.py
+++ src/cmdkit/logging.py
@@ -6,24 +6,27 @@
 
 # type annotations
 from __future__ import annotations
-from typing import Dict, Any, Type
+from typing import Dict, Tuple, Type
 
 # standard libraries
 import uuid
 import socket
+import datetime
 import logging
 
 # internal libs
-from cmdkit.ansi import Ansi
+from cmdkit.ansi import Ansi, COLOR_STDERR
 
 # public interface
 __all__ = ['Logger', 'HOSTNAME', 'INSTANCE',
            'level_color', 'level_by_name',
-           'logging_styles', 'DEFAULT_LOGGING_STYLE', ]
+           'logging_styles', 'DEFAULT_LOGGING_STYLE',
+           'NOTSET', 'DEVEL', 'TRACE', 'DEBUG', 'INFO', 'NOTICE', 'WARNING', 'ERROR', 'CRITICAL']
 
 
 # Cached for later use
 HOSTNAME = socket.gethostname()
+HOSTNAME_SHORT = HOSTNAME.split('.', 1)[0]
 
 
 # Unique for every instance of the program
@@ -56,6 +59,15 @@ DEVEL: int = 1
 logging.addLevelName(DEVEL, 'DEVEL')
 
 
+# Re-export for convenience
+NOTSET: int = logging.NOTSET
+DEBUG: int = logging.DEBUG
+INFO: int = logging.INFO
+WARNING: int = logging.WARNING
+ERROR: int = logging.ERROR
+CRITICAL: int = logging.CRITICAL
+
+
 level_by_name: Dict[str, int] = {
     'NOTSET': logging.NOTSET,
     'DEVEL': DEVEL,
@@ -66,6 +78,31 @@ level_by_name: Dict[str, int] = {
     'WARNING': logging.WARNING,
     'ERROR': logging.ERROR,
     'CRITICAL': logging.CRITICAL,
+}
+
+
+DEFAULT_LOGGING_STYLE = 'default'
+logging_styles = {
+    'default': {
+        'format': ('%(ansi_bold)s%(ansi_level)s%(levelname)8s%(ansi_reset)s %(ansi_faint)s[%(name)s]%(ansi_reset)s'
+                   ' %(message)s'),
+    },
+    'system': {
+        'format': '%(asctime)s.%(msecs)03d %(hostname)s %(levelname)8s [%(app_id)s] [%(name)s] %(message)s',
+    },
+    'detailed': {
+        'format': ('%(ansi_faint)s%(asctime)s.%(msecs)03d %(hostname)s %(ansi_reset)s'
+                   '%(ansi_level)s%(ansi_bold)s%(levelname)8s%(ansi_reset)s '
+                   '%(ansi_faint)s[%(name)s]%(ansi_reset)s %(message)s'),
+    },
+    'detailed-compact': {
+        'format': ('%(ansi_faint)s%(elapsed_hms)s [%(hostname_short)s] %(ansi_reset)s'
+                   '%(ansi_level)s%(ansi_bold)s%(levelname)8s%(ansi_reset)s '
+                   '%(ansi_faint)s[%(relative_name)s]%(ansi_reset)s %(message)s'),
+    },
+    'short': {
+        'format': '[%(ansi_level)s%(levelname)s%(ansi_reset)s] %(message)s',
+    }
 }
 
 
@@ -93,9 +130,12 @@ class Logger(logging.Logger):
         return logging.getLogger(name)
 
     @classmethod
-    def default(cls: Type[Logger], name: str, *args, **kwargs) -> Logger:
+    def default(cls: Type[Logger],
+                name: str = '',
+                format: str = logging_styles['default']['format'],
+                **kwargs) -> Logger:
         """Calls standard `logging.basicConfig` and returns logger."""
-        logging.basicConfig(*args, **kwargs)
+        logging.basicConfig(format=format, **kwargs)
         return cls.with_name(name)
 
 
@@ -103,48 +143,63 @@ class Logger(logging.Logger):
 logging.setLoggerClass(Logger)
 
 
+def solve_relative_time(elapsed: float) -> Tuple[float, int, datetime.timedelta, str]:
+    """
+    Multiple formats of relative time since `elapsed` seconds.
+    Returns:
+        - Relative time in seconds (i.e., `elapsed`)
+        - Relative time in milliseconds
+        - Relative time as `datetime.timedelta`
+        - Relative time in dd-hh:mm:ss.sss format
+    """
+    elapsed_ms = int(elapsed * 1000)
+    reltime_delta = datetime.timedelta(seconds=elapsed)
+    reltime_delta_hours, remainder = divmod(reltime_delta.seconds, 3600)
+    reltime_delta_minutes, reltime_delta_seconds = divmod(remainder, 60)
+    reltime_delta_milliseconds = int(reltime_delta.microseconds / 1000)
+    return (
+        elapsed,
+        elapsed_ms,
+        reltime_delta,
+        f'{reltime_delta.days:02d}-{reltime_delta_hours:02d}:{reltime_delta_minutes:02d}:'
+        f'{reltime_delta_seconds:02d}.{reltime_delta_milliseconds:03d}'
+    )
+
+
 class LogRecord(logging.LogRecord):
-    """Extends LogRecord to include the hostname, app_id, and ANSI color codes."""
+    """Extends standard `logging.LogRecord` to include ANSI colors, time formats, and other attributes."""
 
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
+
+        # Context attributes
         self.app_id = INSTANCE
         self.hostname = HOSTNAME
-        self.ansi_level = level_color.get(self.levelname, Ansi.NULL).value
-        self.ansi_reset = Ansi.RESET.value
-        self.ansi_bold = Ansi.BOLD.value
-        self.ansi_faint = Ansi.FAINT.value
-        self.ansi_italic = Ansi.ITALIC.value
-        self.ansi_underline = Ansi.UNDERLINE.value
-        self.ansi_black = Ansi.BLACK.value
-        self.ansi_red = Ansi.RED.value
-        self.ansi_green = Ansi.GREEN.value
-        self.ansi_yellow = Ansi.YELLOW.value
-        self.ansi_blue = Ansi.BLUE.value
-        self.ansi_magenta = Ansi.MAGENTA.value
-        self.ansi_cyan = Ansi.CYAN.value
-        self.ansi_white = Ansi.WHITE.value
+        self.hostname_short = HOSTNAME_SHORT
+        self.relative_name = self.name.split('.', 1)[-1]
+
+        # Formatting attributes
+        self.ansi_level = level_color.get(self.levelname, Ansi.NULL).value if COLOR_STDERR else ''
+        self.ansi_reset = Ansi.RESET.value if COLOR_STDERR else ''
+        self.ansi_bold = Ansi.BOLD.value if COLOR_STDERR else ''
+        self.ansi_faint = Ansi.FAINT.value if COLOR_STDERR else ''
+        self.ansi_italic = Ansi.ITALIC.value if COLOR_STDERR else ''
+        self.ansi_underline = Ansi.UNDERLINE.value if COLOR_STDERR else ''
+        self.ansi_black = Ansi.BLACK.value if COLOR_STDERR else ''
+        self.ansi_red = Ansi.RED.value if COLOR_STDERR else ''
+        self.ansi_green = Ansi.GREEN.value if COLOR_STDERR else ''
+        self.ansi_yellow = Ansi.YELLOW.value if COLOR_STDERR else ''
+        self.ansi_blue = Ansi.BLUE.value if COLOR_STDERR else ''
+        self.ansi_magenta = Ansi.MAGENTA.value if COLOR_STDERR else ''
+        self.ansi_cyan = Ansi.CYAN.value if COLOR_STDERR else ''
+        self.ansi_white = Ansi.WHITE.value if COLOR_STDERR else ''
+
+        # Timing attributes
+        (self.elapsed,
+         self.elapsed_ms,
+         self.elapsed_delta,
+         self.elapsed_hms) = solve_relative_time(self.relativeCreated / 1000)
 
 
 # Register new LogRecord implementation
 logging.setLogRecordFactory(LogRecord)
-
-
-DEFAULT_LOGGING_STYLE = 'default'
-logging_styles = {
-    'default': {
-        'format': ('%(ansi_bold)s%(ansi_level)s%(levelname)8s%(ansi_reset)s %(ansi_faint)s[%(name)s]%(ansi_reset)s'
-                   ' %(message)s'),
-    },
-    'system': {
-        'format': '%(asctime)s.%(msecs)03d %(hostname)s %(levelname)8s [%(app_id)s] [%(name)s] %(message)s',
-    },
-    'detailed': {
-        'format': ('%(ansi_faint)s%(asctime)s.%(msecs)03d %(hostname)s %(ansi_reset)s'
-                   '%(ansi_level)s%(ansi_bold)s%(levelname)8s%(ansi_reset)s '
-                   '%(ansi_faint)s[%(name)s]%(ansi_reset)s %(message)s'),
-    },
-    'short': {
-        'format': '[%(ansi_level)s%(levelname)s%(ansi_reset)s] %(message)s',
-    }
-}
